8ca5a1b9b355eb4f62668dfa846b5ef74a9aff0f
James Won <me@jwon.me>
2015-06-06 09:36:52 -0700

    turning get_data into a lib

--- get_data.py
+++ get_data.py
@@ -1,9 +1,23 @@
 import json
 import requests
+import pprint
 
-r = requests.get('https://data.smcgov.org/api/views/4uz3-p92v/rows.json?accessType=DOWNLOAD')
 
-data = r.json()
-first_wifi_hotspot = data['data'][0]
+def get_data():
+    # Get data
+    r = requests.get('https://data.smcgov.org/api/views/4uz3-p92v/rows.json?accessType=DOWNLOAD')
+    data = r.json()
 
-print first_wifi_hotspot
+    # Extract column names
+    columns = [ thing['name'] for thing in data['meta']['view']['columns'] ]
+
+    results = []
+    for hotspot in data['data']:
+        hotspot_dict = dict(zip(columns, hotspot))
+        results.append(hotspot_dict)
+
+    return results
+
+if __name__ == '__main__':
+    wifi_data = get_data()
+    pprint.pprint(wifi_data)
