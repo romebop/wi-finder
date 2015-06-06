2ff40633c21ded185e3bde1eab45819b321ef6d3
James Won <me@jwon.me>
2015-06-06 12:58:37 -0700

    INTEGRATION OF THE YELP_SCAPER + WEBAPP

--- wi-finder.py
+++ wi-finder.py
@@ -1,13 +1,24 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
+import yelp_scrape
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-	return 'Hello World!'
+@app.route('/wifi')
+def wifi_results():
+    latitude = request.args.get('latitude', '')
+    longitude = request.args.get('longitude', '')
+
+    if latitude == '' or longitude == '':
+        return 'Cannot get coordinates'
+
+    yelp_results = yelp_scrape.get_yelp(latitude, longitude)
+
+    return str(yelp_results)
 
 if __name__ == '__main__':
-	app.run()
+    app.run(debug=True)
--- yelp_scrape.py
+++ yelp_scrape.py
@@ -4,28 +4,29 @@ import json
 import pprint
 
 
-
-latitude = 37.58975467000005
-longitide = -122.31838430299996
+#latitude = 37.58975467000005
+#longitide = -122.31838430299996
 
 # dictionary structure - {(latitude, longitude): (url, name)}
 def output(yelp_json):
-	outDict = {}
-	for business in yelp_json["businesses"]:
-		if business["is_closed"] == False:
-			outDict[(business["location"]["coordinate"]["latitude"], business["location"]["coordinate"]["longitude"])] = (business["url"], business["name"])
-	return outDict
+    outDict = {}
+    for business in yelp_json["businesses"]:
+        if business["is_closed"] == False:
+            outDict[(business["location"]["coordinate"]["latitude"], business["location"]["coordinate"]["longitude"])] = (business["url"], business["name"])
+    return outDict
 
 # return unique list of restaurant names and urls
 def get_yelp(latitude, longitide):
-	search_terms = ["restaurants", "cafe", "coffee", "business"]
-	#for i in range(0, len(search_terms)):
-	query = {'term': search_terms[0], 'll':str(latitude) + ',' + str(longitide), 'sort':'1', 'limit':20}
-	r = requests.get("http://api.yelp.com/v2/search/", params=query, auth = auth)
-	out = output(r.json())
-	return out
+        search_terms = ["restaurants", "cafe", "coffee", "business"]
+        #for i in range(0, len(search_terms)):
+        query = {'term': search_terms[0], 'll':str(latitude) + ',' + str(longitide), 'sort':'1', 'limit':20}
+        r = requests.get("http://api.yelp.com/v2/search/", params=query, auth = auth)
+        out = output(r.json())
+        return out
 
-a = get_yelp(latitude, longitide)
-pprint.pprint(a)
+
+if __name__ == '__main__':
+    a = get_yelp(latitude, longitide)
+    pprint.pprint(a)
 
 
