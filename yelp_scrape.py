cc36c192b13b77c6c62b9f6c09d0b42cce1454d0
Nader Azari <njazari@berkeley.edu>
2015-06-06 12:34:23 -0700

    made it one function to call

--- yelp_scrape.py
+++ yelp_scrape.py
@@ -8,15 +8,6 @@ import pprint
 latitude = 37.58975467000005
 longitide = -122.31838430299996
 
-# return unique list of restaurant names and urls
-def get_yelp(latitude, longitide):
-	search_terms = ["restaurants", "cafe", "coffee", "business"]
-	#for i in range(0, len(search_terms)):
-	query = {'term': search_terms[0], 'll':str(latitude) + ',' + str(longitide), 'sort':'1', 'limit':20}
-	r = requests.get("http://api.yelp.com/v2/search/", params=query, auth = auth)
-	return r.json()
-
-a = get_yelp(latitude, longitide)
 # dictionary structure - {(latitude, longitude): (url, name)}
 def output(yelp_json):
 	outDict = {}
@@ -25,13 +16,16 @@ def output(yelp_json):
 			outDict[(business["location"]["coordinate"]["latitude"], business["location"]["coordinate"]["longitude"])] = (business["url"], business["name"])
 	return outDict
 
-out = output(a)
-print len(out)
+# return unique list of restaurant names and urls
+def get_yelp(latitude, longitide):
+	search_terms = ["restaurants", "cafe", "coffee", "business"]
+	#for i in range(0, len(search_terms)):
+	query = {'term': search_terms[0], 'll':str(latitude) + ',' + str(longitide), 'sort':'1', 'limit':20}
+	r = requests.get("http://api.yelp.com/v2/search/", params=query, auth = auth)
+	out = output(r.json())
+	return out
 
-"""
-pprint.pprint(a["businesses"][0]["location"]["coordinate"]["latitude"]) #latitide
-pprint.pprint(a["businesses"][0]["location"]["coordinate"]["longitude"]) #longitude
-pprint.pprint(a["businesses"][0]["url"])
-pprint.pprint(a["businesses"][0]["is_closed"]) #open or closed
-pprint.pprint(a["businesses"][0]["name"]) #name
-"""
+a = get_yelp(latitude, longitide)
+pprint.pprint(a)
+
+
