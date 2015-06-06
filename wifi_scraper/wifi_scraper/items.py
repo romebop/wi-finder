be868cd82b3642caf620f6b1253c82c9dd6854fb
rome-bop <park.srome@gmail.com>
2015-06-06 14:42:30 -0700

    adding wifi_scraper stuffs

---
+++ wifi_scraper/wifi_scraper/items.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class WifiScraperItem(scrapy.Item):
+    short_def_list = scrapy.Field()
